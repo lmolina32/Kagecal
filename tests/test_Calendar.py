9c4d0162371f955f6e333a94639c37b6d8f260c0
Leonardo Molina <lmolina3@nd.edu>
2026-04-14 15:13:08 -0500

    feat: create more tests

--- tests/test_Calendar.py
+++ tests/test_Calendar.py
@@ -1,36 +1,9 @@
 #!/usr/bin/env python3
 
 import pytest
-from typing import Optional
-from datetime import datetime, timezone, timedelta
 
-from DistributedCalendar.Calendar import Calendar, Repeats, Event, Day
-
-
-def create_repeat(day: Day = Day.SUNDAY, hour: int = 1) -> Repeats:
-    now_utc = int(datetime.now(timezone.utc).timestamp())
-    one_hour_later_utc = int(
-        (datetime.now(timezone.utc) + timedelta(hours=hour)).timestamp()
-    )
-    return Repeats(day, now_utc, one_hour_later_utc)
-
-
-def create_event(
-    name: str = "progress_report",
-    start: int = 1713045600,
-    end: int = 1713045601,
-    description: Optional[str] = None,
-    location: Optional[str] = None,
-    repeats: Optional[Repeats] = None,
-) -> Event:
-    return Event(
-        name=name,
-        start=start,
-        end=end,
-        description=description,
-        location=location,
-        repeats=repeats,
-    )
+from utils import create_event, create_repeat
+from DistributedCalendar.Calendar import Calendar, Day
 
 
 def test_repeats_eq() -> None:
--- tests/test_PersistentCalendar.py
+++ tests/test_PersistentCalendar.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import pytest
-from unittest.mock import MagicMock, mock_open
+import pickle
+from unittest.mock import MagicMock, mock_open, patch
 
+from utils import create_event, create_repeat
 from DistributedCalendar.Calendar import Calendar
-from DistributedCalendar.PersistantCalendar import PersistantHashTable
+from DistributedCalendar.PersistantCalendar import PersistantHashTable, Transaction
 
 
 @pytest.fixture
@@ -12,16 +14,12 @@ def calendar(mocker, tmp_path):
     txn_log = tmp_path / "calendar.txns"
     ckpt = tmp_path / "calendar.ckpt"
     new_ckpt = tmp_path / "calendar.new.ckpt"
-    txn_log.touch()
-    ckpt.touch()
-    new_ckpt()
 
-    return_cal = Calendar()
-    mocker.patch.object(PersistantHashTable, "_restore", return_value=return_cal)
     mocker.patch.object(PersistantHashTable, "CKPT_PATH", str(ckpt))
     mocker.patch.object(PersistantHashTable, "NEW_CKPT_PATH", str(new_ckpt))
     mocker.patch.object(PersistantHashTable, "TXN_LOG_PATH", str(txn_log))
     calendar = PersistantHashTable()
+    calendar.calendar = Calendar()
     return calendar
 
 
@@ -48,3 +46,113 @@ def test_init_txns_logged_starts_at_zero(mocker):
     pht = PersistantHashTable()
 
     assert pht.txns_logged == 0
+
+
+def test_persistent_restore_checkpoint_logic(calendar, tmp_path, mocker) -> None:
+    ckpt = tmp_path / "calendar.ckpt"
+    ckpt.touch()
+
+    # assert that checkpoint reads in the file correctly
+    c = Calendar()
+    for i in range(10):
+        c.create(**create_event(start=i).__dict__)
+    assert len(c.events) == 10
+
+    ckpt.write_bytes(pickle.dumps(c.events))
+    restored_calendar = calendar._restore()
+    assert len(restored_calendar.events) == 10
+
+
+def test_persistent_restore_new_checkpoint_logic(calendar, tmp_path, mocker) -> None:
+    new_ckpt = tmp_path / "calendar.new.ckpt"
+    new_ckpt.touch()
+
+    # assert that new_checkpoint reads calles _checkpoint
+    m = mocker.patch.object(
+        PersistantHashTable, "_checkpoint", return_value=MagicMock()
+    )
+    calendar._restore()
+    m.assert_called_once()
+    mocker.stop(m)
+    assert new_ckpt.exists() is not True
+
+
+def test_persistent_restore_transaction_creates_logic(calendar, tmp_path) -> None:
+    txn_log = tmp_path / "calendar.txns"
+    idents = []
+    txn_log.touch()
+    # assert that txn logs work
+    with txn_log.open("wb") as f:
+        for i in range(10):
+            event = create_event(start=i)
+            idents.append(hash(event))
+            f.write(pickle.dumps(Transaction("create", hash(event), event)) + b"\n")
+
+    restored_calendar = calendar._restore()
+    assert len(restored_calendar.events) == 10
+    assert calendar.txns_logged == 10
+    for i in range(len(idents)):
+        assert idents[i] in restored_calendar.events
+
+
+def test_persistent_restore_transaction_creates_deletes_logic(
+    calendar, tmp_path
+) -> None:
+    txn_log = tmp_path / "calendar.txns"
+    idents = []
+    txn_log.touch()
+    # assert that txn logs work
+    with txn_log.open("wb") as f:
+        for i in range(10):
+            event = create_event(start=i)
+            idents.append(hash(event))
+            f.write(pickle.dumps(Transaction("create", hash(event), event)) + b"\n")
+        for i in range(4):
+            f.write(pickle.dumps(Transaction("delete", idents[i], None)) + b"\n")
+
+    restored_calendar = calendar._restore()
+    assert len(restored_calendar.events) == 6
+    assert calendar.txns_logged == 14
+
+    for i in range(4, len(idents)):
+        assert idents[i] in restored_calendar.events
+    txn_log.unlink()
+
+
+def test_persistent_restore_transaction_CDM_logic(calendar, tmp_path):
+    "CDM -> create, delete, modify"
+    txn_log = tmp_path / "calendar.txns"
+    idents = []
+    modifies = []
+    txn_log.touch()
+    # assert that txn logs work
+    with txn_log.open("wb") as f:
+        for i in range(10):
+            event = create_event(start=i)
+            idents.append(hash(event))
+            f.write(pickle.dumps(Transaction("create", hash(event), event)) + b"\n")
+        for i in range(4):
+            f.write(pickle.dumps(Transaction("delete", idents[i], None)) + b"\n")
+        for i in range(4, len(idents)):
+            event = create_event(start=10 + i)
+            modifies.append(hash(event))
+            f.write(pickle.dumps(Transaction("modify", idents[i], event)) + b"\n")
+
+    restored_calendar = calendar._restore()
+    assert len(restored_calendar.events) == 6
+    assert calendar.txns_logged == 20
+
+    for i in range(len(idents)):
+        assert idents[i] not in restored_calendar.events
+
+    for i in range(len(modifies)):
+        assert modifies[i] in restored_calendar.events
+    txn_log.unlink()
+
+
+def test_persistent_log(calendar, tmp_path) -> None:
+    pass
+
+
+def test_persistent_checkpoint(calendar, tmp_path) -> None:
+    pass
