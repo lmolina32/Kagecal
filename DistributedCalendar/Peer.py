78df52135f7874b5db918573478cc2f430483dd8
Leonardo Molina <lmolina3@nd.edu>
2026-04-20 08:53:03 -0500

    feat: add peer class

---
+++ DistributedCalendar/Peer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+class Peer:
+    def __init__(self) -> None:
+        self.logical_clock: int = 0
+
+    
--- DistributedCalendar/__init__.py
+++ DistributedCalendar/__init__.py
@@ -1,2 +1,5 @@
 from .Calendar import Calendar
 from .PersistantCalendar import PersistantHashTable
+from .Server import Server
+from .Client import Client
+from .Peer import Peer
