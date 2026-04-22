7a120a9b5d3a68d20c8b72710ad4c79568c74238
Leonardo Molina <lmolina3@nd.edu>
2026-04-22 14:20:36 -0500

    fix: get rid of the chopped code, add argument parser

--- kagecal.py
+++ kagecal.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env
 
 import sys
+import argparse
 
 from DistributedCalendar.Peer import Peer
 
-if len(sys.argv) != 3:
-    print(f"Usage {sys.argv[0]} <calendar-project> <peer-name>", file=sys.stderr)
-    sys.exit(1)
 
-peer = Peer(calendar_name=sys.argv[1], peer_name=sys.argv[2])
-peer.run()
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--calendar-project",
+        help="The specific calendar you intend to subscribe to",
+        required=True,
+    )
+    parser.add_argument(
+        "--peer-name", help="the peer name you want to register as", required=True
+    )
+    args = parser.parse_args()
+
+    peer = Peer(calendar_name=args.calendar_project, peer_name=args.peer_name)
+    peer.run()
+
+
+if __name__ == "__main__":
+    main()
