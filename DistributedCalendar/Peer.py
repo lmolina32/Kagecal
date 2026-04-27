b8d5a357e69e5d04a9db9667e50b820a7afd06a1
Leonardo Molina <lmolina3@nd.edu>
2026-04-27 10:05:29 -0500

    chore: finish peer modules, create, modify, delete, start/stop server, and add init bootstrap

--- DistributedCalendar/Peer.py
+++ DistributedCalendar/Peer.py
@@ -13,6 +13,7 @@ from typing import TypedDict, Optional
 from .Server import Server, ServerMode, ServerFlags
 from .Client import Client
 from .PersistantCalendar import PersistantCalendar
+from .Calendar import Repeats
 
 log_format = "[%(levelname)s %(asctime)s %(module)s:%(lineno)d] %(message)s"
 logging.basicConfig(
@@ -51,6 +52,7 @@ class Peer:
         self.election_happening = False
         self.election_cv = threading.Condition()
         self.pause_server = threading.Event()
+        self._server_thread: Optional[threading.Thread] = None
 
         ckpt: str = f"calendar_{self.calendar_ident}_{self.peer_ident}.ckpt"
         txn: str = f"calendar_{self.calendar_ident}_{self.peer_ident}.txn"
@@ -65,6 +67,12 @@ class Peer:
             leader_port=0,
         )
         self.own_port = self.server.port
+
+        self._bootstrap()
+
+    def _bootstrap(self) -> None:
+        # TODO: add more detail docstring
+        """Discover the existing peers and deterine the intial role of this node"""
 
         peer_list = sorted(
             self._get_catalog(), key=lambda x: x["lastheardfrom"], reverse=True
@@ -103,12 +111,12 @@ class Peer:
                 self.server.leader_host, self.server.leader_port = (
                     client.who_is_leader()
                 )
+                self.server.set_mode(ServerMode.FOLLOWER)
                 self.log.info(
                     f"Leader found at {self.server.leader_host}:{self.server.leader_port}"
                 )
-                self.server.set_mode(ServerMode.FOLLOWER)
                 break
-            except ConnectionError:
+            except TimeoutError:
                 self.log.error(
                     f"Cannot reach peer {target_peer_ident} at {target_port}:{target_host}"
                 )
@@ -129,9 +137,9 @@ class Peer:
                 self.own_port,
             )
             # TODO: ensure when calling sync on the leader this is the tuple order returned
-            leaders_calendar, logcial_clock = client.sync()
-            self.server.update(leaders_calendar, logical_clock)
-        except ConnectionError:
+            calendar, logcial_clock = client.sync()
+            self.server.update(calendar, logcial_clock)
+        except TimeoutError:
             self.call_election()
 
     def _get_catalog(self) -> list[CatalogEntry]:
@@ -159,17 +167,104 @@ class Peer:
 
         return peers
 
-    def create(self) -> Optional[int]:
-        """If this peer is the leader, takes the lock on the calendar and updates it directly. Otherwise, uses the RPC stub on the client to update the calendar on the server, then syncs with the server."""
-        # grab the server look
-        # fail release the lock,
-        pass
-
-    def delete(self): ...
-
-    def modify(self): ...
-
     # TODO: Write all the calendar stubs, make the mutating ones wait on the election condition variable.
+    # TODO: reads automatically happen in the kagecal, should we lock?
+    # TODO: figure out what will be passed down from the UI thread into RPC methods for peer
+    def create(
+        self,
+        name: str,
+        start: int,
+        end: int,
+        description: Optional[str],
+        location: Optional[str],
+        repeats: Optional[Repeats],
+    ) -> Optional[int]:
+        """Grab the calendar lock first. If this peer is the leader, update the calendar directly then broadcast sync. Otherwise, use RPC stub on the client to send update to leader, then update local calendar to skip broadcast sync. If RPC stub on client fails catch the exception and raise an election"""
+        try:
+            with self.server.calendar_lock:
+                match self.server.mode:
+                    case ServerMode.FOLLOWER:
+                        # TODO: should we just keep open one socket connection for the leader?
+                        client = Client(
+                            self.peer_ident,
+                            self.server.leader_host,
+                            self.server.leader_port,
+                            self.own_host,
+                            self.own_port,
+                        )
+                        client.create(name, start, end, description, location, repeats)
+                        event_id = self.server.persistence.create(
+                            name, start, end, description, location, repeats
+                        )
+                    case ServerMode.LEADER:
+                        event_id = self.server.persistence.create(
+                            name, start, end, description, location, repeats
+                        )
+                        self.server.broadcast_clock()
+        except TimeoutError:
+            self.log.error("Leader unreachable during create(); calling election")
+            self.call_election()
+        return event_id
+
+    def delete(self, ident: int) -> None:
+        """Grab the calendar lock first. If this peer is the leader, update the calendar directly then broadcast sync. Otherwise, use RPC stub on the client to send update to leader, then update local calendar to skip broadcast sync. If RPC stub on client fails catch the exception and raise an election"""
+        try:
+            with self.server.calendar_lock:
+                match self.server.mode:
+                    case ServerMode.FOLLOWER:
+                        client = Client(
+                            self.peer_ident,
+                            self.server.leader_host,
+                            self.server.leader_port,
+                            self.own_host,
+                            self.own_port,
+                        )
+                        client.delete(ident)
+                        self.server.persistence.delete(ident)
+                    case ServerMode.LEADER:
+                        self.server.persistence.delete(ident)
+                        self.server.broadcast_clock()
+        except TimeoutError:
+            self.log.error("Leader unreachable during delete(); calling election")
+            self.call_election()
+
+    def modify(
+        self,
+        ident: int,
+        name: str,
+        start: int,
+        end: int,
+        description: Optional[str],
+        location: Optional[str],
+        repeats: Optional[Repeats],
+    ) -> Optional[int]:
+        """Grab the calendar lock first. If this peer is the leader, update the calendar directly then broadcast sync. Otherwise, use RPC stub on the client to send update to leader, then update local calendar to skip broadcast sync. If RPC stub on client fails catch the exception and raise an election"""
+        try:
+            with self.server.calendar_lock:
+                match self.server.mode:
+                    case ServerMode.FOLLOWER:
+                        client = Client(
+                            self.peer_ident,
+                            self.server.leader_host,
+                            self.server.leader_port,
+                            self.own_host,
+                            self.own_port,
+                        )
+                        client.modify(
+                            ident, name, start, end, description, location, repeats
+                        )
+                        updated_id = self.server.persistence.modify(
+                            ident, name, start, end, description, location, repeats
+                        )
+                    case ServerMode.LEADER:
+                        updated_id = self.server.persistence.modify(
+                            ident, name, start, end, description, location, repeats
+                        )
+                        self.server.broadcast_clock()
+        except TimeoutError:
+            self.log.error("Leader unreachable during modify(); calling election")
+            self.call_election()
+        return updated_id
 
     def _serve(self):
         """Target for the thread created by start_server."""
@@ -186,12 +281,20 @@ class Peer:
             if flags & ServerFlags.DO_SYNC:
                 # Sync is a request to the leader, so it could fail, triggering an election.
                 try:
-                    calendar, clock = self.client.sync()
+                    client = Client(
+                        self.peer_ident,
+                        self.server.leader_host,
+                        self.server.leader_port,
+                        self.own_host,
+                        self.own_port,
+                    )
+                    calendar, clock = client.sync()
+                    self.server.update(calendar, clock)
                 except TimeoutError:
+                    self.log.error("Sync with leader failed, calling election")
                     self.call_election()
-                    continue
-                self.server.update(calendar, clock)
 
+    # TODO: who ever calls an election is responsbile for create client socket for the leader
     def call_election(self):
         """Initiates (or continues) the leader election protocol. While this method is executing, this peer will stop serving incoming requests, and attempts to modify the calendar state will block, but local reads will not block. A new client to the new leader is created."""
         # TODO: Complete this method
@@ -253,20 +356,45 @@ class Peer:
                     except TimeoutError:
                         continue
                     self.server.update(events, logical_clock)
+                    break
 
         # 3. Wake the UI thread if it is waiting for the election to finish to place write.
         with self.election_cv:
             self.election_happening = False
             self.election_cv.notify()
 
-    def start_server():
+    def start_server(self):
         """Creates a background thread that serves incoming requests to the peer's server, and syncs the local calendar with the leader's calendar in the event of an update."""
         # TODO: Still need to add actual background thread at init
-        pass
+        if self._server_thread is not None and self._server_thread.is_alive():
+            self.log.warning(
+                "start_server() called when server thread is still running"
+            )
+            return
 
-    def stop_server():
+        self.pause_server.clear()
+        self._server_thread = threading.Thread(target=self._serve, daemon=True)
+        self._server_thread.start()
+        self.log.info(f"Server thread start for peer {self.peer_ident}")
+
+    def stop_server(self):
         """Stops a background thread that serves incoming requests to the peer's server, and syncs the local calendar with the leader's calendar in the event of an update."""
-        pass
+        self.pause_server.set()
+        self._server_thread.join()
+        # should this be a loop and check if we are leader or recieve sink to break out
+        try:
+            client = Client(
+                self.peer_ident,
+                self.server.leader_host,
+                self.server.leader_port,
+                self.own_host,
+                self.own_port,
+            )
+            calendar, lock = client.sync()
+            self.server.update(calendar, lock)
+        except TimeoutError:
+            self.call_election()
+        self.log.info(f"server Thread stopped for peer {self.peer_ident}")
 
 
 if __name__ == "__main__":
